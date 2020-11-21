1c1e0f12e42fabe96ba1ce333fc1c469fb3bcd20
tamir-a <66635271+tamir-a@users.noreply.github.com>
2020-11-22 03:15:03 +0800

    11.22

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -9,7 +9,7 @@ import {
   ON_SUCCESS_BUY_USER,
 } from "../_actions/types";
 
-export default function (state = {}, action) {
+ export default function (state = {}, action) {
   switch (action.type) {
     case REGISTER_USER:
       return { ...state, register: action.payload };
